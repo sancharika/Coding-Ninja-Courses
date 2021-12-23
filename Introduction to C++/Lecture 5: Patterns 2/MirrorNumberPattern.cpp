9c13c82b9ee7eb352d382a54f65e91941c90d403
Sancharika Debnath <80142595+sancharika@users.noreply.github.com>
2021-12-23 15:26:07 +0530

    Create MirrorNumberPattern.cpp

---
+++ Introduction to C++/Lecture 5: Patterns 2/MirrorNumberPattern.cpp
@@ -0,0 +1,76 @@
+/*
+Mirror Number Pattern
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+
+
+. . .1 
+. . 1 2 
+. 1 2 3 
+1 2 3 4
+The dots represent spaces.
+
+
+
+Input format :
+
+Integer N (Total no. of rows)
+
+Output format :
+
+Pattern in N lines
+
+Constraints
+
+0 <= N <= 50
+
+Sample Input 1:
+
+3
+
+Sample Output 1:
+
+    1 
+   12
+  123
+
+Sample Input 2:
+
+4
+
+Sample Output 2:
+
+   1 
+  12
+ 123
+1234
+
+*/
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  int n,k=0;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j=1;j<=(n-i);j++)
+        cout<<" ";
+        for (int l = 1; l <=i; l++){
+            while (k<=(i-1)){
+                cout<<k+1;
+                k++;
+            }
+        }
+        k=0;
+        cout<<endl;
+    }
+    return 0;
+}
+
+
