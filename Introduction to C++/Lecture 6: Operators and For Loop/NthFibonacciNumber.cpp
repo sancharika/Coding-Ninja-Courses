aa78fbd8ff727ed9d03bd9fe6f5e72a8d064c701
Sancharika Debnath <80142595+sancharika@users.noreply.github.com>
2021-12-23 15:33:55 +0530

    Create NthFibonacciNumber.cpp

---
+++ Introduction to C++/Lecture 6: Operators and For Loop/NthFibonacciNumber.cpp
@@ -0,0 +1,64 @@
+/*
+Nth Fibonacci Number 
+Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+
+    F(n) = F(n-1) + F(n-2), 
+    Where, F(1) = F(2) = 1
+
+Provided N you have to find out the Nth Fibonacci Number.
+Input Format :
+
+The first line of each test case contains a real number ‘N’.
+
+Output Format :
+
+For each test case, return its equivalent Fibonacci number.
+
+Constraints:
+
+1 <= N <= 10000     
+Where ‘N’ represents the number for which we have to find its equivalent Fibonacci number.
+
+Time Limit: 1 second
+
+Sample Input 1:
+
+6
+
+Sample Output 1:
+
+8
+
+Explanation of Sample Input 1:
+
+Now the number is ‘6’ so we have to find the “6th” Fibonacci number
+So by using the property of the Fibonacci series i.e 
+
+[ 1, 1, 2, 3, 5, 8]
+So the “6th” element is “8” hence we get the output.
+
+*/
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    //Write your code here.    
+    int n,sum=0,t1=1,t2=1;
+    cin>>n;
+    	if(n==1||n==2)
+            cout<<t1;
+      else
+      {
+            for(int i=3;i<=n;i++)
+            {
+                sum=t1+t2;
+                t1=t2;
+                t2=sum;
+            }
+     cout<<sum;
+      }
+    
+    return 0;
+}
